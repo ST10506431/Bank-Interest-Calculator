cacf981db143b0fd90c7e3b44e687b492b10c48f
ST10506431 <ST10506431@myemeris.edu.za>
2026-03-05 19:45:22 +0200

    Init commit

---
+++ BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bmicalculator;
+
+/**
+ *
+ * @author hazel
+ */
+
+/*
+Activity 3: Time Converter
+Objective: Learn how to perform unit conversions using multiplication.
+Steps to Follow:
+1. Create a new Java project called TimeConverter.
+2. Import the Scanner class at the beginning.
+3. Inside the main method, do the following:
+o Ask the user to enter a time in hours.
+o Convert it into:
+Minutes = hours × 60
+Seconds = hours × 3600
+4. Print the results.
+Activity 4: Bank Interest Calculator
+Objective: Learn how to apply an interest formula.
+Steps to Follow:
+1. Create a new Java project called BankInterestCalculator.
+2. Import the Scanner class at the beginning.
+3. Inside the main method, do the following:
+o Ask the user to enter:
+▪ Principal amount (P)
+▪ Rate of interest (R)
+▪ Time in years (T)
+o Use the formula:
+Simple Interest = (P × R × T) / 100
+o Multiply the principal, rate, and time, then divide by 100.
+o Display the interest earned.
+*/
+public class BMICalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
